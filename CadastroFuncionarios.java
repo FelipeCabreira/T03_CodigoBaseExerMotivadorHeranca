b7661047706ed7982f80474b558f0545298174b3
12107429 <cabreira92@gmail.com>
2019-04-08 20:44:07 -0300

    Exercicio sem heranca

--- CadastroFuncionarios.java
+++ CadastroFuncionarios.java
@@ -4,12 +4,12 @@ public class CadastroFuncionarios{
     private ArrayList<Funcionario> funcionarios;
 
     private void carregaFuncionarios(){
-        funcionarios.add(new Funcionario("A3212","Huguinho Pato",1890.0));
-        funcionarios.add(new Funcionario("A3312","Zezinho Pato",3720.0));
-        funcionarios.add(new Funcionario("A3412","Luizinho Pato",8940.0));
-        funcionarios.add(new Funcionario("B9931","Lala Pata",4220.0));
-        funcionarios.add(new Funcionario("B9932","Lele Pata",1283.0));
-        funcionarios.add(new Funcionario("B9933","Lili Pata",12438.0));
+        funcionarios.add(new Funcionario("A3212","Huguinho Pato",1890.0,false,1));
+        funcionarios.add(new Funcionario("A3312","Zezinho Pato",3720.0,false,2));
+        funcionarios.add(new Funcionario("A3412","Luizinho Pato",8940.0,true,2));
+        funcionarios.add(new Funcionario("B9931","Lala Pata",4220.0,false,1));
+        funcionarios.add(new Funcionario("B9932","Lele Pata",1283.0,true,3));
+        funcionarios.add(new Funcionario("B9933","Lili Pata",12438.0,true,1));
     }
 
     public CadastroFuncionarios(){
--- Funcionario.java
+++ Funcionario.java
@@ -1,42 +1,76 @@
-public class Funcionario{
+public class Funcionario {
     public static final double LIM_ISENCAO_IR = 2000;
     private String matricula;
     private String nome;
     private double salarioBruto;
+    private boolean areaDeRisco;
+
+
+
     
-    public Funcionario(String matricula, String nome, double salarioBruto){
+    private int tipoPesquisador;
+
+    public Funcionario(String matricula, String nome, double salarioBruto, boolean areaDeRisco, int tipoPesquisador) {
         this.matricula = matricula;
         this.nome = nome;
-        this. salarioBruto = salarioBruto;
+        this.salarioBruto = salarioBruto;
+        this.areaDeRisco = areaDeRisco;
+        this.tipoPesquisador = tipoPesquisador;
     }
 
-	public String getMatricula() {
-		return matricula;
-	}
-	
-	public String getNome() {
-		return nome;
-	}
-	
-	public double getSalarioBruto() {
-		return salarioBruto;
-	}
-    
-    public double getINSS(){
-        return salarioBruto*0.1;
+    public String getMatricula() {
+        return matricula;
     }
 
-    public double getImpRenda(){
-        if (salarioBruto <= LIM_ISENCAO_IR){
+    public String getNome() {
+        return nome;
+    }
+
+    public double getSalarioBruto() {
+        return salarioBruto;
+    }
+
+    public double getINSS() {
+        if (areaDeRisco == true) {
+            double resultadoValor;
+            resultadoValor = salarioBruto * 0.25;
+            this.validaTipo(resultadoValor);
+            return resultadoValor;
+        } else {
+            return salarioBruto * 0.1;
+        }
+
+    }
+
+    public double validaTipo(double resultado) {
+        switch (tipoPesquisador) {
+        case 1:
+            return resultado * 0.05;
+
+        case 2:
+            return resultado * 0.1;
+
+        case 3:
+            return resultado * 0.2;
+
+        default:
+            return -1;
+        }
+    }
+
+    public double getImpRenda() {
+        if (salarioBruto <= LIM_ISENCAO_IR) {
+            // SALARIO LIMPO SEM DESCONTO
             return 0.0;
-        }else{
+        } else {
             double aux = salarioBruto - LIM_ISENCAO_IR;
             double ir = aux * 0.2;
+            // IMPORTO DE RENDA LIMPO TIRANDO 20% PORQ EXCEDE O LIMITE
             return ir;
         }
     }
 
-    public double getSalarioLiquido(){
+    public double getSalarioLiquido() {
         return salarioBruto - getINSS() - getImpRenda();
     }
 }
